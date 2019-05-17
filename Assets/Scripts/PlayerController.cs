4857c0c89bb0bfea93f1920e20d78483760db339
Shreyaa Sridhar <shreyaasridhar@Shreyaas-MBP.lan1>
2019-05-16 22:43:57 -0700

    added all the files for Roll a ball

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate() 
+    {
+        float moveVertical = Input.GetAxis("Vertical") ;
+        float moveHorizontal = Input.GetAxis("Horizontal") ;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed) ;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+            winText.text = "HAPPY BIRTHDAY AMMA!!!!!";
+    }
+}
