ddade30aa844d84dc5ea775376badabe41b910e7
Shreyaa Sridhar <shreyaasridhar@Shreyaas-MacBook-Pro.local>
2019-05-17 16:01:17 -0700

    added timer code

--- Python/client.py
+++ Python/client.py
@@ -1,13 +1,22 @@
 #! /usr/local/bin/python3
 import zmq
-import time
+import sched,time
+from  datetime import datetime
 import random
+import numpy as np
+def do_PCA(pos):
+	print("PCA's")
+	time.sleep(5)
+	print(pos)
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:12346")
 
 TIMEOUT = 10000
 
+
+pos = np.array([])
+s = datetime.now()
 while True:
 	socket.send_string("request")
 	poller = zmq.Poller()
@@ -17,8 +26,16 @@ while True:
 		if evt.get(socket) == zmq.POLLIN:
 			response = socket.recv(zmq.NOBLOCK)
 			print(response)
+			#print(pos)
+			pos = np.append(pos,response)
+			#print(pos)
+			if (datetime.now() - s).total_seconds() >= 3:
+				do_PCA(pos)
+				pos = np.array([])
+				s = datetime.now()
 			continue
 	time.sleep(0.5)
 	socket.close()
 	socket = context.socket(zmq.REQ)
 	socket.connect("tcp://localhost:12346")
+
