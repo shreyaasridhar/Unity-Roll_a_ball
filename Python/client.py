a505809c97ccc37babf9ba406a30d0e7a45b501d
Kyle Wang <kylewang@udel.edu>
2019-05-17 17:16:58 -0700

    compute PCA for every 5 seconds

--- Python/client.py
+++ Python/client.py
@@ -1,21 +1,29 @@
 #! /usr/local/bin/python3
 import zmq
-import sched,time
+import time
 from  datetime import datetime
-import random
 import numpy as np
-def do_PCA(pos):
-	print("PCA's")
-	time.sleep(5)
-	print(pos)
+from sklearn.decomposition import PCA
+
+def do_PCA(pos,ct):
+	print("Calculating PCA")
+	data = pos[:ct,:]
+	ipca = PCA(n_components=3, svd_solver='full') #arpack
+	ipca.fit(data)
+	print(ipca.transform(data))
+	#print(ipca.get_covariance(data))
+	return None
+
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:12346")
 
 TIMEOUT = 10000
 
+n = 5 # total second amount for data block
+ct = 0 # index  for pos
+pos = np.zeros([n*3000,3])
 
-pos = np.array([])
 s = datetime.now()
 while True:
 	socket.send_string("request")
@@ -25,14 +33,18 @@ while True:
 	if evt:
 		if evt.get(socket) == zmq.POLLIN:
 			response = socket.recv(zmq.NOBLOCK)
-			print(response)
-			#print(pos)
-			pos = np.append(pos,response)
-			#print(pos)
-			if (datetime.now() - s).total_seconds() >= 3:
-				do_PCA(pos)
-				pos = np.array([])
+			#print(response)
+
+			chars = response.decode("utf-8")
+			pos[ct] = list(map(float,chars.split(" ")))
+			ct += 1
+
+
+			if (datetime.now() - s).total_seconds() >= n:
+				do_PCA(pos,ct)
 				s = datetime.now()
+				pos = np.zeros([n * 3000, 3])
+				ct = 0
 			continue
 	time.sleep(0.5)
 	socket.close()
